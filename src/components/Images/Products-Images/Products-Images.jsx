8ca937e702ee3ba47c130e84711776f4b8245d7f
Yakir13 <yakir.helfer100@gmail.com>
2024-08-03 11:40:31 +0300

    products

--- src/components/Images/Products-Images/Products-Images.jsx
+++ src/components/Images/Products-Images/Products-Images.jsx
@@ -2,16 +2,104 @@ import React from 'react'
 import './Products-Images.css'
 
 function ProductsImages() {
-    return (
-    <div className='container-images'>
-        <h2>Our Products</h2> 
-        <div className='images'>
-            <img src="/src/assets/pic_1.jpg" alt="pic_1" />
-            <img src="/src/assets/pic_2.jpg" alt="pic_2" />
-            <img src="/src/assets/pic_3.jpg" alt="pic_3" />
-            <img src="/src/assets/pic_4.jpg" alt="pic_4" />    
+    return(
+    <>
+    <h2>Our <span>Products</span></h2>
+    <div className='container'>
+        <div className="gallery-container w-3 h-3">
+            <div className="gallery-item">
+                <div className="image">
+                    <img className='gallery-container' src="/src/assets/pic_1.jpg" alt="pic_1" />
+                </div>
+                <div className="text"><b>Computer specifications:</b> <br />
+                    <b>CPU:</b> AMD Ryzen 9 7900X or Intel Core i9-13900K <br />
+                    <b>Graphic Crad:</b> NVIDIA GeForce RTX 4080 16GB or AMD Radeon RX 7900 XT <br />
+                    <b>RAM:</b> 32 GB (2 x 16 GB) <br />
+                    <b>Motherboard:</b> ASUS ROG Strix X670E-E Gaming or MSI MPG Z790 Edge <br />
+                    <b>Power Supply Unit:</b> Wattage: 850W <br />
+                    <b>Case:</b> NZXT H510 Elite or Corsair iCUE 5000X RGB
+                </div>
+            </div>
+        </div>
+        <div className="gallery-container w-3 h-3">
+            <div className="gallery-item">
+                <div className="image">
+                    <img className='gallery-container' src="/src/assets/pic_2.jpg" alt="pic_2" />
+                </div>
+                <div className="text"><b>Computer specifications:</b> <br />
+                    <b>CPU:</b> AMD Ryzen 9 7900X or Intel Core i9-13900K <br />
+                    <b>Graphic Crad:</b> NVIDIA GeForce RTX 4080 16GB or AMD Radeon RX 7900 XT <br />
+                    <b>RAM:</b> 32 GB (2 x 16 GB) <br />
+                    <b>Motherboard:</b> ASUS ROG Strix X670E-E Gaming or MSI MPG Z790 Edge <br />
+                    <b>Power Supply Unit:</b> Wattage: 850W <br />
+                    <b>Case:</b> NZXT H510 Elite or Corsair iCUE 5000X RGB
+                </div>
+            </div>
+        </div>
+        <div className="gallery-container w-3 h-3">
+            <div className="gallery-item">
+                <div className="image">
+                    <img className='gallery-container' src="/src/assets/pic_3.jpg" alt="pic_3" />
+                </div>
+                <div className="text"><b>Computer specifications:</b> <br />
+                    <b>CPU:</b> AMD Ryzen 9 7900X or Intel Core i9-13900K <br />
+                    <b>Graphic Crad:</b> NVIDIA GeForce RTX 4080 16GB or AMD Radeon RX 7900 XT <br />
+                    <b>RAM:</b> 32 GB (2 x 16 GB) <br />
+                    <b>Motherboard:</b> ASUS ROG Strix X670E-E Gaming or MSI MPG Z790 Edge <br />
+                    <b>Power Supply Unit:</b> Wattage: 850W <br />
+                    <b>Case:</b> NZXT H510 Elite or Corsair iCUE 5000X RGB
+                </div>
+            </div>
+        </div>
+        <div className="gallery-container w-3 h-3">
+            <div className="gallery-item">
+                <div className="image">
+                    <img className='gallery-container' src="/src/assets/pic_4.jpg" alt="pic_4" />
+                </div>
+                <div className="text"><b>Computer specifications:</b> <br />
+                    <b>CPU:</b> AMD Ryzen 9 7900X or Intel Core i9-13900K <br />
+                    <b>Graphic Crad:</b> NVIDIA GeForce RTX 4080 16GB or AMD Radeon RX 7900 XT <br />
+                    <b>RAM:</b> 32 GB (2 x 16 GB) <br />
+                    <b>Motherboard:</b> ASUS ROG Strix X670E-E Gaming or MSI MPG Z790 Edge <br />
+                    <b>Power Supply Unit:</b> Wattage: 850W <br />
+                    <b>Case:</b> NZXT H510 Elite or Corsair iCUE 5000X RGB
+                </div>
+            </div>
+        </div>
+        <div className="gallery-container w-3 h-3">
+            <div className="gallery-item">
+                <div className="image">
+                    <img className='gallery-container' src="/src/assets/pic_5.jpg" alt="pic_5" />
+                </div>
+                <div className="text">Office Setup 5</div>
+            </div>
+        </div>
+        <div className="gallery-container w-3 h-3">
+            <div className="gallery-item">
+                <div className="image">
+                    <img className='gallery-container' src="/src/assets/pic_6.jpg" alt="pic_6" />
+                </div>
+                <div className="text">Office Setup 6</div>
+            </div>
+        </div>
+        <div className="gallery-container w-3 h-3">
+            <div className="gallery-item">
+                <div className="image">
+                    <img className='gallery-container' src="/src/assets/pic_7.jpg" alt="pic_7" />
+                </div>
+                <div className="text">Office Setup 7</div>
+            </div>
+        </div>
+        <div className="gallery-container w-3 h-3">
+            <div className="gallery-item">
+                <div className="image">
+                    <img className='gallery-container' src="/src/assets/pic_8.jpg" alt="pic_8" />
+                </div>
+                <div className="text">Office Setup 8</div>
+            </div>
         </div>
     </div>
+    </>
     )
 }
 
