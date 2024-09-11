75006e182f3fedfd9d8d7086a2a1fa6f923d4751
Yakir13 <yakir.helfer100@gmail.com>
2024-09-11 23:31:28 +0300

    fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ContactContent from '../components/Content/Contact-Content/Contact-Content';
-import ContactForm from '../components/From/Contact-Form/Contact-Form';
+import ContactForm from '../components/Form/Contact-Form/Contact-Form';
 
 function Contact(){
     return(
