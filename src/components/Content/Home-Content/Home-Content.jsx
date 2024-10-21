bb8d1b329e561b1da92b9c17492e8f51eb4c66f1
Yakir13 <yakir.helfer100@gmail.com>
2024-10-21 21:34:37 +0300

    home

--- src/components/Content/Home-Content/Home-Content.jsx
+++ src/components/Content/Home-Content/Home-Content.jsx
@@ -4,7 +4,7 @@ import './Home-Content.css'
 function HomeContent(){
     return(
     <div className="home">
-        <h1>Choose Your Setup</h1>
+        <a href="Products">Choose Your Setup</a>
         <p>At Setup Universe, we specialize in creating ideal setups for tech, workspaces, and gaming. Whether you need cutting-edge technology, ergonomic office solutions, or top-tier gaming gear, we’ve got the expertise and products to make it happen!</p>
     </div>
     )
